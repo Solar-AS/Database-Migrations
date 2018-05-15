6cebdb860042b4f8420ce4c5ea65355b3872399b
Daniel González García <danielgonnet@gmail.com>
2018-05-15 14:41:44 +0200

    add migration script

---
+++ run-migrations.ps1
@@ -0,0 +1,1 @@
+packages\FluentMigrator.Console.3.0.0\net461\x64\Migrate.exe -db SqlServer2014  -a src\Db.fluentmigrator\bin\Debug\Db.fluentmigrator.dll -conn "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=fluentmigrator;Integrated Security=True;" -t migrate:up
