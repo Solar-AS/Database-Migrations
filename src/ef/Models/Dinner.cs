5a61e9787ae5be072e039a88e925075f764b0716
Daniel González García <danielgonnet@gmail.com>
2018-05-15 14:59:54 +0200

    add model project

---
+++ src/ef/Models/Dinner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ef.Models
+{
+	public class Dinner
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public DateTimeOffset EventDate { get; set; }
+
+		public string Description { get; set; }
+		public string HostedBy { get; set; }
+		public string ContactPhone { get; set; }
+		public string Address { get; set; }
+		public string Country { get; set; }
+	}
+}
